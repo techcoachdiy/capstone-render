15ed5fbc3432ddc1b26676c96a52be13aede5f39
Hsingwen <hsingwen.wang@gmail.com>
2026-04-14 08:56:16 +0400

    Fix admin registration for all models

--- womenhealth/admin.py
+++ womenhealth/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Topic, FoodComponent, Entry, Revision, Recipe 
+
+admin.site.register(Topic)
+admin.site.register(FoodComponent)
+admin.site.register(Entry)
+admin.site.register(Revision)
+admin.site.register(Recipe)
